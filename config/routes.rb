37ad5757aa456f07df53374dcf7ad3fda502b5ca
mccarthy417nahant <kevinmccarthy01@comcast.net>
2018-12-22 20:17:33 -0500

    style forms and add images to show and index pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :listings
+  get 'pages/about'
+  get 'pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'listings#index'
 end
